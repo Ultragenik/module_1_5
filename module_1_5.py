f3d1e04686526b9e6068e5e3958ab433d4fdbeb9
Ultragenik <ultragenik@gmail.com>
2024-10-11 15:59:19 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 'paris', 'france', 1, 3
+print(immutable_var)
+mutable_list = ['car', 'banana',1,2,3]
+print(mutable_list)
+mutable_list[4]= 'train'
+print(mutable_list)
+immutable_var[0] = 'moscow'
