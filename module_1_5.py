143fa3027ab2564a3c6e2ebc416f1d09293ef908
Ultragenik <ultragenik@gmail.com>
2024-10-11 16:10:31 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,7 +1,7 @@
 immutable_var = 'paris', 'france', 1, 3
 print(immutable_var)
+# immutable_var[0] = 'moscow' в кортеже нельзя изменять данные, в отличии от строк
 mutable_list = ['car', 'banana',1,2,3]
 print(mutable_list)
 mutable_list[4]= 'train'
 print(mutable_list)
-immutable_var[0] = 'moscow'
